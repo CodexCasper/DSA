e2f88205d5c313312a2d67646f05b75e2a4c2da3
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-02-15 15:35:26 +0530

    Time: 15 ms (20.17%), Space: 26.1 MB (48.27%) - LeetHub

--- LeetCode/Medium/1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ LeetCode/Medium/1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
+//TC:O(n*(log(max(nums))))
     int smallestDivisor(vector<int>& nums, int threshold) {
-        int n = nums.size();
         int low = 1 , high = *max_element(nums.begin(),nums.end());
-        int ans = -1;
 
+        int ans = -1;
         while( low <= high ){
-            int mid = ( low + high )/ 2;
+            int mid = ( low + high ) / 2;
             int sum = 0;
-            for(int i = 0 ; i < n ; i++){
-                sum += ceil((double)nums[i] / mid);
-            }
+            for(int i= 0 ; i < nums.size() ; i++) sum += ceil((double)nums[i] / mid);
+
             if( sum <= threshold ){
-                ans  = mid;
+                ans = mid;
                 high = mid - 1;
             }
             else low = mid + 1;
-        }
+        } 
         return ans;
     }
 };
