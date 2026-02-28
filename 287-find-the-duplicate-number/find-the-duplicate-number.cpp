dc392f4b9458c882c5a308776686209d41793a61
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-02-28 18:04:43 +0530

    Time: 12 ms (41.67%) | Memory: 65.1 MB (68.75%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-//TC:O(n) , SC:O(n)
     int findDuplicate(vector<int>& nums) {
+        /*  //TC:O(n) , SC:O(n)
         unordered_map<int,int> hash;
 
         for(int num: nums){
@@ -10,5 +10,26 @@ public:
             hash[num] = 1;
         }
         return -1;
+         */
+        //TC:O(n) , SC:O(1)
+        int slow = 0 , fast = 0;
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+            if(slow == fast){
+                break;
+            }
+        } while(slow != fast);
+
+        int n1 = 0;
+        int n2 = slow;
+
+        while(n1 != n2){
+            n1 = nums[n1];
+            n2 = nums[n2];
+        }
+        return n1;
     }
 };
