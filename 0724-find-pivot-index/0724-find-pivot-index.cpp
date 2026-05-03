5f3ab9d6d4b56d9b20cad8968431b5f615fa65f9
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-05-03 10:39:40 +0530

    Time: 0 ms (100%), Space: 37.1 MB (10.08%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        //we took n+1 bcoz prefix[0] == 0 bcoz there are no elements behind it
-        //  1 2 3 4 5
-        //i        0   1   2   3   4   5
-        //prefix   0   1   3   6  10  15
+        /*
+        1. Goal: Find index i such that
+   sum(0 → i-1) == sum(i+1 → n-1)
 
-        //tC:O(n) , sC:O(2n) == O(n)
+2. Instead of recomputing sums every time:
+   → Precompute prefix and suffix sums
 
+3. prefix[i] = sum of elements before i
+   suffix[i] = sum of elements after i
+
+4. For each index i:
+   if(prefix[i] == suffix[i]) → return i
+
+5. If no such index → return -1
+        */
         int n = nums.size();
+
         vector<int> prefixsum(n+1,0);
         vector<int> suffixsum(n+1,0);
 
-        for(int i = 1 ; i < n ; i++){
+        for(int i = 1; i <= n ; i++){
             prefixsum[i] = prefixsum[i-1] + nums[i-1];
         }
+
         for(int i = n-2 ; i >= 0 ; i--){
             suffixsum[i] = suffixsum[i+1] + nums[i+1];
         }
 
         for(int i = 0 ; i < n ; i++){
-            int prefix = prefixsum[i];
-            int suffix = suffixsum[i];
+            int ans1 = prefixsum[i];
+            int ans2 = suffixsum[i];
 
-            if(prefix == suffix) return i;
+            if(ans1 == ans2) return i;
         }
         return -1;
     }
