12f8d924d911a068c6cccb8e228144cac44068ae
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-05-03 11:25:21 +0530

    Time: 38 ms (73.99%), Space: 45.4 MB (70.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-       /* //TC:O(n*n) , SC:O(1) won't work bcoz of size of n
-        int n = nums.size();
-        int cnt = 0;
-        for(int i = 0 ; i < n ; i++){
-            int sum = 0;
-            for(int j = i ; j < n ; j++){
-                sum += nums[j];
-                if(sum == k){
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
+        //TC:O(n) , SC:O(n)
+        /*
+        1. Brute force checks all subarrays → O(n²), too slow.
+
+2. Use prefix sum:
+   Let sum = sum of elements from 0 → current index.
+
+3. For any subarray ending at current index:
+   sum(i → j) = prefix[j] - prefix[i-1]
+
+4. We need:
+   prefix[j] - prefix[i-1] = k
+   ⇒ prefix[i-1] = prefix[j] - k
+
+5. So at each index:
+   check how many times (sum - k) appeared before.
+
+6. Use hashmap to store:
+   prefix sum → frequency
+
+7. For each element:
+   - update sum
+   - if (sum - k) exists → add its frequency to count
+   - store current sum in map
+
+8. Initialize map with (0 → 1) to handle subarrays starting from index 0.
         */
-        ///TC:O(n) , SC:O(n)
-        unordered_map<int, int> mpp;
+        unordered_map<int , int> mpp;
         mpp[0] = 1;
 
-        int sum = 0;
+        int currsum = 0;
         int cnt = 0;
         for(int x : nums){
-            sum += x;
+            currsum += x;
 
-            if(mpp.find(sum - k) != mpp.end()){
-                cnt += mpp[sum-k];
+            if(mpp.find(currsum - k) != mpp.end()){
+                // we did this instead of +1 bcoz (mpp[currsum-k]) may have appeared multiple times
+                //ie. 1 -1  1 -1  , k = 0
+                //i   0  1  2  3
+                //sum 1  0  1  0 
+                //so here if sum = 0 , sum - k = 0 then mpp[0] = 2 which will be wrong for +1
+                cnt += mpp[currsum-k];
             }
-
-            mpp[sum]++;
+            mpp[currsum]++;
         }
         return cnt;
     }
