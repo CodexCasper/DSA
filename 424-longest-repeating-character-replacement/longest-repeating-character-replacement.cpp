00a66919c87c0f6cb08bc7837c3b78d0b8ff859d
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-05-25 16:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (69.91%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        //TC:O(N) , SC:O(26)
+        vector<int> freq(26,0);
+
+        int maxlen = 0;
+        int maxfreq = 0;
+        int l = 0 , r = 0;
+        
+        while(r < s.size()){
+            freq[s[r] - 'A']++;
+
+            maxfreq = max(maxfreq , freq[s[r] - 'A']);
+
+            if((r - l + 1) - maxfreq > k){
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            maxlen = max(maxlen,r-l+1);
+            r++;
+        }
+        return maxlen;
+    }
+};
