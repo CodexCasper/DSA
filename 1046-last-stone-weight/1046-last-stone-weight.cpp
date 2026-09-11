d978bba37ba59592f07f25cef466a3f6ed8005ed
Parth Bindal <parth.23mip10104@vitbhopal.ac.in>
2026-09-11 10:31:12 +0530

    Time: 0 ms (100%), Space: 9.9 MB (79.69%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+//TC:O(n log n) , SC:O(n)
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxheap;
+
+        for(int stone: stones) {
+            maxheap.push(stone);
+        }
+
+        while(maxheap.size() > 1) {
+
+            int first = maxheap.top();
+            maxheap.pop();
+
+            int second = maxheap.top();
+            maxheap.pop();
+
+            if(first != second) {
+                maxheap.push(first - second);
+            }
+        }
+        if(maxheap.empty()) {
+            return 0;
+        }
+
+        return maxheap.top();
+    }
+};
